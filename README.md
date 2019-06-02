a8ce1ce6c66924f81a1e8eed24b8f48caa03bf3e
szmoon <szmoon@gmail.com>
2019-06-01 17:02:54 -0700

    add endpoints

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # flask-mysql-docker-api
 Python/Flask tasks API using a Docker container and remotely hosted MySQL database.
 
-###Methods
-Create Task
-Get Task
-List Tasks
-Update Task
-Delete Task
-
-**Docker Commands:**
+###Project Setup
 ```
 docker-compose up --build
 ```
+
+###Methods
+**Get (all) Tasks**
+- GET: http://localhost:5000
+
+**Create Task**
+- POST: http://localhost:5000/?taskName=[TASK_NAME]
+
+**Get Task**
+- GET: http://localhost:5000/[TASK_ID]
+
+**Update Task**
+- PATCH: http://localhost:5000/[TASK_ID]?taskName=[TASK_NAME]
+
+**Delete Task**
+- DELETE: http://localhost:5000/[TASK_ID]
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 from flask_mysqldb import MySQL
 from config import mysqlHost, mysqlUser, mysqlPassword, mysqlDb
 
@@ -10,10 +10,52 @@ app.config['MYSQL_PASSWORD'] = mysqlPassword
 app.config['MYSQL_DB'] = mysqlDb
 
 mysql = MySQL(app)
-    
-@app.route('/', methods=['GET')
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'this is an api'
+    # create task
+    if request.method == 'POST':
+        taskName = request.args.get('taskName')
+        if not taskName:
+            return 'param taskName is required to create task'
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO tasks(task_name) VALUES (%s)', [taskName])
+        mysql.connection.commit()
+        cur.close()
+        return 'POST success'
+    # list all tasks
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT task_name from tasks')
+        results = cur.fetchall()
+        return str(results)
+
+@app.route('/<taskId>', methods=['GET', 'PATCH', 'DELETE'])
+def taskById(taskId):
+    # get one task
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT task_name from tasks WHERE task_id = (%s)', [taskId])
+        results = cur.fetchone()
+        return str(results)
+    # update task
+    if request.method == 'PATCH':
+        taskName = request.args.get('taskName')
+        if not taskName:
+            return 'param taskName is required to update task'
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE tasks set task_name = (%s) WHERE task_id = (%s)', [taskName, taskId])
+        mysql.connection.commit()
+        cur.close()
+        return 'updated task with id: %s' % taskId
+    # delete task
+    if request.method == 'DELETE':
+        cur = mysql.connection.cursor()
+        cur.execute('DELETE from tasks WHERE task_id = (%s)', [taskId])
+        mysql.connection.commit()
+        cur.close()
+        return 'deleted task with id of %s' % taskId
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
